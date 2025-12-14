9c9a6e9d80181a929454a6a7bd17830dad57cf97
Yousefharara <hrarty37@gmail.com>
2025-12-14 17:36:08 +0200

    - feat(ProductsPage): adding products details

--- src/pages/ProductsPage/index.jsx
+++ src/pages/ProductsPage/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import CartItems from "../../components/CartItems";
+import FilterCategory from "../../components/FilterCategory";
+import Products from "../../components/Products";
+import Container from "../../components/Container";
+import "./style.css";
 
 const ProductsPage = () => {
-    return (
-        <div>
-            <h2>Products page</h2>
+  return (
+    <div className="products__page">
+      <Container>
+        <div className="products__page__content">
+          <div className="title">
+            <h3>Simple E-commerce Store</h3>
+            <p>React Training: useState & useEffect Demo</p>
+          </div>
+          <CartItems />
+          <FilterCategory />
+          <Products />
         </div>
-    );
-}
+      </Container>
+    </div>
+  );
+};
 
 export default ProductsPage;
