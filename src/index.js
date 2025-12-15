f667498c6d483964ce97bd494740faa0bb7ae463
Yousefharara <hrarty37@gmail.com>
2025-12-15 18:55:53 +0200

    - refactore(index.js): delete strick mode - refactore(index.css): modifing and add style for container

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./index.css";
+import ProductProvider from "./context/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ProductProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </ProductProvider>
 );
