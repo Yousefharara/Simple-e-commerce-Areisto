b8d356ddbbd27863524dd124c0047c837b88b4a2
Yousefharara <hrarty37@gmail.com>
2025-12-15 18:49:36 +0200

    - feat(api.js): adding constant values for api

---
+++ src/constant/api.js
@@ -0,0 +1,14 @@
+export const API_ACTION = {
+  DATA: "DATA",
+  PRODUCTS: "PRODUCT",
+  CATEGORIES: 'CATEGORIES',
+  IS_LOADING: "IS_LOADING",
+  ERROR: "ERROR",
+};
+
+export const API_PATH = {
+    GET_ALL_PRODCUTS: 'products?limit=194',
+    GET_PRODCUTS: 'products',
+    GET_GATEGORIES: 'products/categories',
+    GET_PRODUCTS_BY_CATEGORY: 'products/category/:name'
+}
